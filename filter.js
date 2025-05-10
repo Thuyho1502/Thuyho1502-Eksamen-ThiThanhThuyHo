451612cec6e390dca532fb03a49beaf75ae3e54b
Thuyho1502 <p2thanhthuy1502@gmail.com>
2025-05-11 00:57:59 +0200

    filter random user based on gender and age

--- filter.js
+++ filter.js
@@ -4,26 +4,79 @@ const matchedBtn = document.getElementById("matchedDatingBtn");
 const matchedSection = document.getElementById("matchedSection");
 const randomuser = document.getElementById("randomUser");
 const matchContainer = document.getElementById("matchContainer");
+
+const genderSelect = document.getElementById("genderSelect");
+const minAgeInput = document.getElementById("minAge");
+const maxAgeInput= document.getElementById("maxAge");
+
 const viewProfileBtn = document.getElementById("viewProfileBtn");
 
-// Hiển thị khu vực matched khi nhấn "Matched Dating"
 matchedBtn.addEventListener("click", () => {
     matchedSection.classList.remove("hidden");
-    // Có thể ẩn các khu vực khác nếu cần, ví dụ:
     document.getElementById("userProfile").classList.add("hidden");
+    loadFilterSetting();
 });
 
-async function fetchUser() {
-    let response = await fetch("https://randomuser.me/api/");
-    let data = await response.json();
-    console.log(data);
-    return data.results[0];
+randomuser.addEventListener("click", showRandomFilterUser );
+
+[genderSelect, minAgeInput, maxAgeInput].forEach(input =>{
+    input.addEventListener("change",saveFilterSettings);
+});
+function saveFilterSettings(){
+    const filters ={
+        gender : genderSelect.value,
+        minAgeInput : parseInt(minAgeInput.value),
+        maxAgeInput : parseInt(maxAgeInput.value)
+    };
+    localStorage.setItem("userFilters", JSON.stringify(filters));
 }
 
-randomuser.addEventListener("click", showRandomUser);
+function loadFilterSetting(){
+    const saved = JSON.parse(localStorage.getItem("userFilters"));
+    if(saved){
+        genderSelect.value = saved.gender || "";
+        minAgeInput.value = saved.minAgeInput ||18;
+        maxAgeInput.value = saved.maxAgeInput ||100;
 
-async function showRandomUser() {
-    const user = await fetchUser(); 
+    }
+}
+
+
+async function fetchUser() {
+    try{
+        const response = await fetch("https://randomuser.me/api/");
+        const data = await response.json();
+        return data.results[0];
+    }
+    catch(error){
+        console.log("Fetch failed:", error);
+        return null;
+    }
+}
+
+async function showRandomFilterUser() {
+ const filter ={
+    gender: genderSelect.value,
+    minAge : parseInt(minAgeInput.value),
+    maxAge: parseInt(maxAgeInput.value)
+ }; 
+
+  let user; 
+
+do{
+    user = await fetchUser(); 
+    
+}
+
+ while(
+    (filter.gender && user.gender !== filter.gender) ||
+    user.dob.age < filter.minAge ||
+    user.dob.age > filter.maxAge
+        
+    
+ )
+  console.log("Matched user:", user);
+ 
  
  const card = document.createElement("div");
     card.classList.add("card");
@@ -37,8 +90,7 @@ async function showRandomUser() {
         <p>Phone: ${user.phone}</p>
     `;
 
-    card.classList.add("card");
-
     matchContainer.innerHTML = "";
     matchContainer.appendChild(card);
 }
+ 
